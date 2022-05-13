40d71dca128f97b089fac8bb5d10d1e1e25863a7
Ruslana Pavliuk <pavliuk_r_v@protonmail.com>
2022-05-13 10:00:52 +0300

    home work8 after refinement

--- code.js
+++ code.js
@@ -1,36 +1,32 @@
 const buttonAdd = document.querySelector("#addNewToDO");
+const listToDo = document.querySelector('.listToDo');
 
-buttonAdd.addEventListener('click', function (event){
-    newToDo = document.querySelector('#newToDo').value;
-    //console.log(newToDo);
+buttonAdd.addEventListener('click', function(event) {
+    let newToDo = document.querySelector('#newToDo').value;
     let li = document.createElement('li');
     li.innerHTML = newToDo;
     document.querySelector('ul').append(li);
     document.querySelector('#newToDo').value = ' ';
 
-    addButton ()
-    //deleteButtons()
-
+    addButton()
 })
 
-
-
-const listToDo = document.querySelector('.listToDo');
-listToDo.addEventListener( 'click', function(event){
-    if (event.target.closest('li')){
+listToDo.addEventListener( 'click', function(event) {
+    if (event.target.closest('li')) {
         event.target.closest('li').classList.toggle('done');
     }
-    if (event.target.closest('button')){
 
+    if (event.target.closest('button')) {
         event.target.previousElementSibling.remove();
         event.target.remove();
     }
 })
 
-
-function addButton (){
+function addButton() {
     let deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete';
     deleteButton.classList.add('delete_button');
-    document.querySelector('li:last-child').after(deleteButton);
+
+    let liLasChild = document.querySelector('li:last-child');
+    liLasChild.after(deleteButton);
 }
